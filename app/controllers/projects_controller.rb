617df82f69eeb000260a1675320608c41434c016
Andrés Howard <ahowardm@gmail.com>
2025-05-13 09:22:28 -0400

    feat: crud for projects

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,47 @@
+class ProjectsController < ApplicationController
+  before_action :set_project, only: [ :show, :edit, :update, :destroy ]
+  def index
+    @projects = Project.all
+  end
+
+  def show
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    project = Project.new project_params
+    if project.save
+      redirect_to projects_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @project.update project_params
+      redirect_to projects_path
+    else
+      redirect_to edit_project_path(@project)
+    end
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path
+  end
+
+  private
+    def project_params
+      params.require(:project).permit(:name)
+    end
+
+    def set_project
+      @project = Project.find params[:id]
+    end
+end
